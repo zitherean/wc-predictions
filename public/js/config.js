d006cf87a9388ab93f8335ffc7557582ac190c6c
zitherean <155881038+zitherean@users.noreply.github.com>
2026-05-25 20:38:11 +0200

    add config public keys

---
+++ public/js/config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://fmaubjfmbrvgyjvhrqxg.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtYXViamZtYnJ2Z3lqdmhycXhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MjA1NjIsImV4cCI6MjA5NTI5NjU2Mn0.N76dPxbvQ7-NvXUPvVHDG5vCW1H7GbQ4EtSR2nZyi_Y";

12349c8da8be70cfe80c946e595b126fdbe3d7e2
zitherean <155881038+zitherean@users.noreply.github.com>
2026-05-25 20:24:06 +0200

    add more test matches

--- sql/test-matches.sql
+++ sql/test-matches.sql
@@ -22,5 +22,21 @@ values
     '2026-06-12 01:00:00+00',
     'Group stage',
     'scheduled'
+  ),
+  (
+    'test_003',
+    'Netherlands',
+    'Japan',
+    '2026-06-18 13:00:00+00',
+    'Group stage',
+    'scheduled'
+  ),
+  (
+    'test_004',
+    'Germany',
+    'Curacao',
+    '2026-06-15 03:00:00+00',
+    'Group stage',
+    'scheduled'
   )
 on conflict (api_match_id) do nothing;

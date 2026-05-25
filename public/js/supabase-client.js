379deb771a61e9b558f7223cf88ac67f9ab111d8
zitherean <155881038+zitherean@users.noreply.github.com>
2026-05-25 22:51:09 +0200

    add supabase client

---
+++ public/js/supabase-client.js
@@ -0,0 +1,4 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
